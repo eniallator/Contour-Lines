95f07a7cb75a78aff2bd9f274e4a67be75bda360
eniallator <eniallatormc@gmail.com>
2019-06-09 15:48:49 +0100

    Implemented the contour lines with circles so far

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
+const points = [{ x: canvas.width / 4, y: canvas.height / 2 }, { x: (canvas.width * 3) / 4, y: canvas.height / 2 }]
+const radiusModifier = 80
+
 ctx.fillStyle = 'black'
 ctx.strokeStyle = 'white'
+ctx.lineWidth = 3
 
 function run() {
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    // Animation code
+    for (const point of points) {
+        const maxDist = Math.max(point.x, point.y, canvas.width - point.x, canvas.height - point.y)
+        let radius = 0
+
+        while ((radius += radiusModifier) <= maxDist) {
+            ctx.beginPath()
+            ctx.arc(point.x, point.y, radius, 0, Math.PI * 2)
+            ctx.stroke()
+        }
+    }
 
     requestAnimationFrame(run)
 }
